375e22197185cae5211487b978e3e8070d6f3eb9
Seanz_18 <99780315+CamenRyder@users.noreply.github.com>
2024-02-08 07:41:28 -0800

    base getX simply

---
+++ ecommerce_brand/lib/core/utils/theme/styles.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce_brand/core/utils/constant/string_utils.dart';
+import 'package:ecommerce_brand/core/utils/theme/colors.dart';
+import 'package:ecommerce_brand/core/utils/theme/typograhpy.dart';
+import 'package:flutter/material.dart';
+
+
+class AppStyles {
+  static const borderRadius = BorderRadius.all(Radius.circular(3));
+  static const inputStyle = InputDecoration(
+    filled: true,
+    fillColor: AppColors.backgroundPrimary,
+    border: OutlineInputBorder(borderRadius: borderRadius),
+    contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+  );
+
+  static const buttonNormalStyle = ButtonStyle(
+      shape: MaterialStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: borderRadius)),
+      textStyle: MaterialStatePropertyAll(AppTypography.headerPrimary),
+      backgroundColor: MaterialStatePropertyAll(AppColors.primary),
+      padding: MaterialStatePropertyAll(EdgeInsets.symmetric(
+          vertical: Constant.paddingVertical,
+          horizontal: Constant.paddingHorizontal)));
+
+  static const borderSideFloatingActionButton = BoxDecoration(
+      color: Color.fromARGB(255, 211, 210, 210),
+      borderRadius: BorderRadius.all(Radius.circular(12)));
+  static const borderComponentFloatingActionButton = BoxDecoration(
+      color: Color.fromARGB(255, 255, 255, 255),
+      borderRadius: BorderRadius.all(Radius.circular(12)));
+}
+
---
+++ ecommerce_brand/lib/core/utils/theme/typograhpy.dart
@@ -0,0 +1,44 @@
+import 'package:ecommerce_brand/core/utils/theme/colors.dart';
+import 'package:flutter/material.dart';
+
+
+class AppTypography {
+  static const headerPrimary = TextStyle(
+      fontSize: 18, fontWeight: FontWeight.w600, color: AppColors.primary);
+
+  static const header = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: AppColors.black
+  );
+
+  static const headerLight = TextStyle(
+      fontSize: 18, color: AppColors.textLight, fontWeight: FontWeight.w600);
+
+  static const bodyRegular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: AppColors.black
+  );
+
+  static const bodyNormal = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textGrey
+  );
+  static const bodyNormalBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.black
+  );
+
+  static const bodyRegularLight = TextStyle(
+      fontSize: 14, fontWeight: FontWeight.normal, color: AppColors.primary);
+
+  static const bodyBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const bodyBoldLight = TextStyle(
+      fontSize: 16, fontWeight: FontWeight.bold, color: AppColors.textLight);
+}
