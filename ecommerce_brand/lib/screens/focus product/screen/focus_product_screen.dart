48d84f43379bca101e2d7b0fffc50d852dcd2af7
Seanz_18 <99780315+CamenRyder@users.noreply.github.com>
2024-02-21 07:19:50 -0800

    update

--- ecommerce_brand/lib/screens/focus product/screen/focus_product_screen.dart
+++ ecommerce_brand/lib/screens/focus product/screen/focus_product_screen.dart
@@ -19,122 +19,131 @@ class _FocusProductScreen extends State<FocusProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(130, 240, 240, 240),
-        body: Center(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: Constant.paddingHorizontal),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        body: GestureDetector(
+          onTap: () {
+            Get.back(); 
+          },
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Color.fromARGB(130, 185, 180, 180),
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: Constant.paddingHorizontal),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Hero(
-                      tag: 'list_${widget.items.name}',
-                      child: SizedBox(
-                        width: 240,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(12),
-                          child: widget.items.image.image(),
-                        ),
-                      )),
-                  Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      GestureDetector(
-                        child: Container(
-                          margin: const EdgeInsets.all(5),
-                          padding: const EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(13),
-                              color: AppColors.backgroundWhite),
-                          height: 48,
-                          width: 48,
-                          child: Assets.iconsIcFeature.svg(),
-                        ),
-                      ),
-                      GestureDetector(
-                        child: Container(
-                          margin: const EdgeInsets.all(5),
-                          padding: const EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(13),
-                              color: AppColors.backgroundWhite),
-                          height: 48,
-                          width: 48,
-                          child: Assets.iconsIcMedia.svg(),
-                        ),
-                      ),
-                      GestureDetector(
-                        child: Container(
-                          margin: const EdgeInsets.all(5),
-                          padding: const EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(13),
-                              color: AppColors.backgroundWhite),
-                          height: 48,
-                          width: 48,
-                          child: Assets.iconsIcTick.svg(),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Get.back() ;  
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.all(5),
-                          padding: const EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(13),
-                              color: AppColors.backgroundWhite),
-                          height: 48,
-                          width: 48,
-                          child: Assets.iconsIcBan.svg(),
-                        ),
+                      Hero(
+                          tag: 'list_${widget.items.name}',
+                          child: SizedBox(
+                            width: 240,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: widget.items.image.image(),
+                            ),
+                          )),
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          GestureDetector(
+                            child: Container(
+                              margin: const EdgeInsets.all(5),
+                              padding: const EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(13),
+                                  color: AppColors.backgroundWhite),
+                              height: 48,
+                              width: 48,
+                              child: Assets.iconsIcFeature.svg(),
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              margin: const EdgeInsets.all(5),
+                              padding: const EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(13),
+                                  color: AppColors.backgroundWhite),
+                              height: 48,
+                              width: 48,
+                              child: Assets.iconsIcMedia.svg(),
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              margin: const EdgeInsets.all(5),
+                              padding: const EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(13),
+                                  color: AppColors.backgroundWhite),
+                              height: 48,
+                              width: 48,
+                              child: Assets.iconsIcTick.svg(),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Get.back() ;  
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.all(5),
+                              padding: const EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(13),
+                                  color: AppColors.backgroundWhite),
+                              height: 48,
+                              width: 48,
+                              child: Assets.iconsIcBan.svg(),
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 20),
+                    width: double.infinity,
+                    height: 90,
+                    padding: const EdgeInsets.all(Constant.paddingHorizontal),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(13),
+                        color: AppColors.backgroundWhite),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "${widget.items.name}",
+                          style: AppTypography.bodyNormal16Black,
+                        ),
+                        Container(
+                          height: 41,
+                          width: 41,
+                          padding: const EdgeInsets.all(1),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: AppColors.backgroundWhite,
+                              border: Border.all(
+                                  width: 0.45, color: AppColors.textGrey)),
+                          child: const Center(
+                            child: Text(
+                              "Brand",
+                              style: AppTypography.bodyRegular,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
                 ],
               ),
-              Container(
-                margin: const EdgeInsets.only(top: 20),
-                width: double.infinity,
-                height: 90,
-                padding: const EdgeInsets.all(Constant.paddingHorizontal),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(13),
-                    color: AppColors.backgroundWhite),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "${widget.items.name}",
-                      style: AppTypography.bodyNormal16Black,
-                    ),
-                    Container(
-                      height: 41,
-                      width: 41,
-                      padding: const EdgeInsets.all(1),
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: AppColors.backgroundWhite,
-                          border: Border.all(
-                              width: 0.45, color: AppColors.textGrey)),
-                      child: const Center(
-                        child: Text(
-                          "Brand",
-                          style: AppTypography.bodyRegular,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
+            )),
           ),
-        )));
+        ));
   }
 }
