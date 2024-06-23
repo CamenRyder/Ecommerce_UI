58be65ec3d8748b43d243e1b6e5b24287be87792
Minh Hieu <mhieusz.23.dkc@gmail.com>
2024-06-23 15:53:11 +0700

    format code items

--- ecommerce_brand/lib/screens/save item into collection/screen/save_into_collection_screen.dart
+++ ecommerce_brand/lib/screens/save item into collection/screen/save_into_collection_screen.dart
@@ -1,12 +1,10 @@
-import 'package:ecommerce_brand/core/utils/theme/assets.gen.dart';
 import 'package:ecommerce_brand/core/utils/theme/colors.dart';
 import 'package:ecommerce_brand/core/utils/theme/styles.dart';
-import 'package:ecommerce_brand/core/utils/theme/typograhpy.dart';
 import 'package:ecommerce_brand/domain/controller/save_into_collection_controller.dart';
 
 import 'package:ecommerce_brand/screens/save%20item%20into%20collection/widgets/body_save_into_collection_widget.dart';
 import 'package:ecommerce_brand/screens/save%20item%20into%20collection/widgets/footer_sliding_panel_widget.dart';
-import 'package:ecommerce_brand/screens/save%20item%20into%20collection/widgets/list_collection_widget.dart';
+import 'package:ecommerce_brand/screens/save%20item%20into%20collection/widgets/panel_sliding_panel_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -44,59 +42,8 @@ class _SaveIntoCollectionScreenState extends State<SaveIntoCollectionScreen> {
             maxHeight: heightScreen,
             borderRadius: AppStyles.borderRadiusPanel,
             panelBuilder: (sc) {
-              return Column(
-                children: [
-                  Assets.iconsIcRulerRack
-                      .svg(color: Colors.white, height: 4, width: 56),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Expanded(
-                      child: Container(
-                          padding: const EdgeInsets.only(
-                              left: 24, right: 24, top: 32, bottom: 12),
-                          decoration: const BoxDecoration(
-                              color: AppColors.backgroundWhite,
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(18),
-                                  topRight: Radius.circular(18))),
-                          child: Column(
-                            children: [
-                              const Center(
-                                child: Text(
-                                  "Save to drawer",
-                                  style: AppTypography.bodyNormal18Black,
-                                ),
-                              ),
-                              Container(
-                                margin:
-                                    const EdgeInsets.only(top: 18, bottom: 12),
-                                height: 50,
-                                width: double.infinity,
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 18, vertical: 3),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(12),
-                                    color: AppColors.grey65),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    const Text(
-                                      "Filter drawers",
-                                      style: AppTypography.bodyBoldlGrey,
-                                    ),
-                                    Assets.iconsIcResearch
-                                        .svg(color: AppColors.textGrey)
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                  child: ListCollectionWidget(
-                                      scrollController: sc))
-                            ],
-                          )))
-                ],
+              return PanelSlidingPanelWidget(
+                scrollController: sc,
               );
             },
             body: const BodySaveIntoCollectionWidget()),
